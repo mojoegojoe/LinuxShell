4194e60f91fbc91e14d93afedcd0e8a258a44922
SorenEdwards <sorenb99@gmail.com>
2022-10-21 13:36:34 -0600

    fixes str_tok errors

--- mysh/strtok.c
+++ mysh/strtok.c
@@ -12,7 +12,7 @@ void execute_command(char *input_buff);
 
 /* PROC FUNCTIONS */
 void runprocess(char **arg_buff, int isBackGround);
-int bckgrnd_check(char ** arg_buff);
+int bckgrnd_check(char **arg_buff);
 
 /* IO */
 int read_input(char *buffer, int buffer_size);
@@ -23,13 +23,12 @@ void get_args(char *str, char **arg_buffer, int arg_buffer_size);
 int command_handler(char **commands, int commands_size, char **arg_buffer, int arg_buff_size, char **command_buff);
 
 /* STRING MANIPULATION */
+char *str_tok_t(char *str, const char *delim);
 char *str_tok(char *str, const char *delim);
 int strcmp(char *str1, char *str2);
 void clear_rest(char **buff, int buff_size, int index);
 void clear_buffer(char buff[], int buff_size, int index);
 void copy_starting(char **buff, char **buff2, int index);
-
-
 
 /* IO REDIRECTION */
 int check_redirection(char *command);
@@ -41,42 +40,45 @@ char **pipe_elements(char *input);
 void piping(char *command);
 
 #define BUFF_SIZE 80
-#define COMMAND_SIZE 6
+#define COMMAND_SIZE 4
 #define EXIT_FAILURE 1
 #define HOME "/home/soren"
 #define BUFFERSIZE 256
-
 
 /********************************************************
 /    @FUNCION print_line
 /    @input   BUFFER - Pointer to a defined char array
 /             delim  - Pointer to an array of delimitors
 /                       function set by arr[2]
-/    @output  bool   - read line pass/fail boolean  
+/    @output  bool   - read line pass/fail boolean
 /    @method  If no root buffer given a error flag is set
-/             while there is space left on the root buffer 
+/             while there is space left on the root buffer
 /                                  we write in user output.
 /
 ********************************************************/
-int print_line(char *BUFFER, char *delim, int fd){
+int print_line(char *BUFFER, char *delim, int fd)
+{
   // write each char of the length defined output to screen
   int i = 0;
-  if(!BUFFER){
+  if (!BUFFER)
+  {
     return 1;
   }
-  while(i<=BUFFERSIZE){
-    if(BUFFER[i] == delim[2]){
+  while (i <= BUFFERSIZE)
+  {
+    if (BUFFER[i] == delim[2])
+    {
       return 0;
     }
     write(fd, &BUFFER[i], 1);
     i++;
   };
-  
+
   return 1;
 }
 // need echo
-// need pwd 
-// need cd 
+// need pwd
+// need cd
 
 // void pwd()
 // {
@@ -95,7 +97,7 @@ int print_line(char *BUFFER, char *delim, int fd){
 // void echo(char *command)
 // {
 //     char *token = command;
-//     token = str_tok(NULL, " \"\n\t\r"); 
+//     token = str_tok(NULL, " \"\n\t\r");
 //     while (token != NULL)
 //     {
 //         printf("%s ",token);
@@ -109,14 +111,14 @@ int print_line(char *BUFFER, char *delim, int fd){
 // {
 //     char *token = command;
 //     token = str_tok(NULL, " \n\t\r");
-//     if(token == NULL) 
+//     if(token == NULL)
 //     {
-//         if(chdir(HOME) != 0) 
+//         if(chdir(HOME) != 0)
 //             perror("Error");
 //     }
 
 //     else if(token[0] == '~')
-//     {   
+//     {
 //         int n = strlen(token);
 //         char *temp = (char *)malloc(sizeof(char) *n);
 //         strcpy(temp,"\0");
@@ -127,55 +129,63 @@ int print_line(char *BUFFER, char *delim, int fd){
 //                 temp[i-1] = token[i];
 //         }
 //         temp[i] ='\0';
-        
+
 //         char *final_path = (char *)malloc(sizeof(char) *(n + strlen(HOME)+12));
 //         strcpy(final_path, HOME);
 //         strcat(final_path, temp);
 //         final_path[n +strlen(HOME)-1] ='\0';
 //         free(temp);
-//         if(chdir(final_path) != 0) 
+//         if(chdir(final_path) != 0)
 //             perror("Error");
 
 //         free(final_path);
 //     }
-    
-//     else if(chdir(token) != 0) 
+
+//     else if(chdir(token) != 0)
 //         perror("Error");
 
 //     return;
 // }
 
-unsigned char str_contains(char* string, int str_len, char* toFind, int f_len)
+unsigned char str_contains(char *string, int str_len, char *toFind, int f_len)
 {
-    unsigned char slen = str_len;
-    unsigned char tFlen = f_len;
-    unsigned char found = 0;
+  unsigned char slen = str_len;
+  unsigned char tFlen = f_len;
+  unsigned char found = 0;
 
-    if( slen >= tFlen )
+  if (slen >= tFlen)
+  {
+    for (unsigned char s = 0, t = 0; s < slen; s++)
     {
-        for(unsigned char s=0, t=0; s<slen; s++)
+      do
+      {
+
+        if (string[s] == toFind[t])
         {
-            do{
-
-                if( string[s] == toFind[t] )
-                {
-                    if( ++found == tFlen ) return 1;
-                    s++;
-                    t++;
-                }
-                else { s -= found; found=0; t=0; }
-
-              }while(found);
+          if (++found == tFlen)
+            return 1;
+          s++;
+          t++;
         }
-        return 0;
+        else
+        {
+          s -= found;
+          found = 0;
+          t = 0;
+        }
+
+      } while (found);
     }
-    else return -1;
+    return 0;
+  }
+  else
+    return -1;
 }
 
 int check_redirection(char *command)
 {
-  unsigned char out = str_contains(command,BUFF_SIZE, ">", 1);
-  unsigned char in = str_contains(command,BUFF_SIZE, "<", 1);
+  unsigned char out = str_contains(command, BUFF_SIZE, ">", 1);
+  unsigned char in = str_contains(command, BUFF_SIZE, "<", 1);
 
   if ((out > 0) && (in > 0))
     return 3;
@@ -192,7 +202,7 @@ int check_redirection(char *command)
 
 int check_piping(char *command)
 {
-  unsigned char is_pipe = str_contains(command,BUFF_SIZE, "|",1);
+  unsigned char is_pipe = str_contains(command, BUFF_SIZE, "|", 1);
   if (is_pipe > 0)
     return 1;
 
@@ -210,13 +220,13 @@ void shell()
     // CHILD_ID = -1;
     // prompt();
     read_input(input_buff, BUFF_SIZE);
-    
-    //printf("\ninput command: %s\n", input_buff);
-    
+
+    // printf("\ninput command: %s\n", input_buff);
+
     char **commands;
     // commands = tokenize(input);
     // for(int i = 0; i < no; i++)
-    //piping(input_buff);
+    // piping(input_buff);
     execute_command(input_buff);
 
     clear_buffer(input_buff, BUFF_SIZE, 0);
@@ -225,7 +235,7 @@ void shell()
 
 void execute_command(char *input_buff)
 {
-  char *commands[] = {"|","<", ">","2>",">>","&"};
+  char *commands[] = {"|", "<", ">", "&"};
   char *arg_buff[BUFF_SIZE];
   char *command_buff[BUFF_SIZE];
 
@@ -239,15 +249,16 @@ void execute_command(char *input_buff)
 
   if (check_piping(input_buff))
   {
-    printf("\n %s \n",input_buff);
+    printf("\n %s \n", input_buff);
     piping(input_buff);
     // fork_pipes(command);
     return;
   }
 
   get_args(input_buff, arg_buff, BUFF_SIZE);
-  
-  bckgrnd_flag = bckgrnd_check(arg_buff);command = command_handler(commands, COMMAND_SIZE, arg_buff, BUFF_SIZE, command_buff);
+
+  bckgrnd_flag = bckgrnd_check(arg_buff);
+  command = command_handler(commands, COMMAND_SIZE, arg_buff, BUFF_SIZE, command_buff);
   command = command_handler(commands, COMMAND_SIZE, arg_buff, BUFF_SIZE, command_buff);
   // if (check_redirection(command))
   // {
@@ -255,23 +266,25 @@ void execute_command(char *input_buff)
   //   return;
   // }
 
-  //printf("\nstatus: %d\n", command);
+  // printf("\nstatus: %d\n", command);
 
-    //   else if(!strcmp(args[no_args-1], "&"))
-    // {
-    //     args[no_args-1] = NULL;
-    //     run(args, no_args, 1);
-    // }
+  //   else if(!strcmp(args[no_args-1], "&"))
+  // {
+  //     args[no_args-1] = NULL;
+  //     run(args, no_args, 1);
+  // }
 
-
-
-  if (command == 0) {
+  if (command == 0)
+  {
     runprocess(arg_buff, bckgrnd_flag);
-  } else if (command == 2) {
+  }
+  else if (command == 2)
+  {
     runprocess(arg_buff, 1);
   }
-  else if (command == 3) {
-    exec_ioredir(arg_buff, command_buff,bckgrnd_flag);
+  else if (command == 3)
+  {
+    exec_ioredir(arg_buff, command_buff, bckgrnd_flag);
     runprocess(arg_buff, 0);
   }
   return;
@@ -316,104 +329,56 @@ void runprocess(char **arg_buff, int isBackGround)
     }
     if (isBackGround != 1)
     {
-      //printf("waiting\n");
+      // printf("waiting\n");
       waitpid(pid, &status, 0);
-    } 
+    }
     // else (
     //   //getpid()
     // )
   }
 }
 
-<<<<<<< HEAD
 // TO DO: EXECUTE io redirection
-=======
-/*
-Supports input and output redirection,
-output redirection with appends and
-error output redirection
- */
-
->>>>>>> 05a9a5de854ae8d9ceacf39f6a5ca63d5310014f
 void exec_ioredir(char **arg_buff, char **command_buff, int bckgrnd_flag)
 {
-  int fd,saved;
 
-  if (strcmp(command_buff[0], ">") == 0 || strcmp(command_buff[0], ">>") == 0)
+  if (strcmp(command_buff[0], ">") == 0)
   {
-    saved = dup(1);
-    if(strcmp(command_buff[0], ">") == 0)
-    {
-      fd = open(command_buff[1], O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
-    }
-    else
-    {
-      fd = open(command_buff[1], O_WRONLY | O_CREAT | O_APPEND, S_IRWXU);
-    }
-
-    if(fd == -1)
-    {
-      perror("File failed to open"\n);
-      exit(EXIT_FAILURE);
-    }
+    int saved = dup(1);
+    int fd = open(command_buff[1], O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
     dup2(fd, 1);
     close(fd);
-    
-    runprocess(arg_buff,bckgrnd_flag);
-    
-    //restores buffer
-    dup2(saved,1);
+
+    runprocess(arg_buff, bckgrnd_flag);
+    // restore buffer
+    dup2(saved, 1);
     close(saved);
   }
   else if (strcmp(command_buff[0], "<") == 0)
   {
-    saved = dup(0);
-    fd = open(command_buff[1], O_RDONLY);
-    if(fd == -1)
-    {
-      perror("File failed to open"\n);
-      exit(EXIT_FAILURE);
-    }
-
+    int saved = dup(0);
+    int fd = open(command_buff[1], O_RDONLY);
     dup2(fd, 0);
     close(fd);
 
-    runprocess(arg_buff,bckgrnd_flag);
-    dup2(saved,0);
-    close(saved);
-  }
-  else if(strcmp(command_buff[0], "2>") == 0)
-  {
-    saved = dup(2);
-    fd = open(command_buff[1], O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
-    if(fd == -1)
-    {
-      perror("File failed to open"\n);
-      exit(EXIT_FAILURE);
-    }
-    dup2(fd, 2);
-    close(fd);
-
-    runprocess(arg_buff,bckgrnd_flag);
-    dup2(saved,2);
+    runprocess(arg_buff, bckgrnd_flag);
+    dup2(saved, 0);
     close(saved);
   }
 
   return;
 }
-/*
-Checks if given argument is to be run in
-the background
-*/
 
-int bckgrnd_check(char ** arg_buff){
+int bckgrnd_check(char **arg_buff)
+{
 
   int i = 0;
-  
-  while(arg_buff[i] != NULL){
+
+  while (arg_buff[i] != NULL)
+  {
     i++;
   }
-  return strcmp(arg_buff[i-1], "&");
+  return strcmp(arg_buff[i - 1], "&");
 }
 
 char **pipe_elements(char *input)
@@ -428,15 +393,15 @@ char **pipe_elements(char *input)
   {
     pipe_args[oof++] = p;
     p = str_tok(NULL, "|");
-    //printf("%s", p);
+    printf("%s", p);
   }
 
   return pipe_args;
 }
 void piping(char *command)
 {
-  //printf("\n passed command \n%s\n", command);
-  
+  // printf("\n passed command \n%s\n", command);
+
   int status;
   char **pipe_args = pipe_elements(command);
   // printf("%s\n",pipe_args[0]);
@@ -446,8 +411,8 @@ void piping(char *command)
 
   for (int j = 0; pipe_args[j] != NULL; j++)
   {
-    //printf("%d\n", j);
-    
+    // printf("%d\n", j);
+
     // printf("%s\n",pipe_args);
     pipe(pipes);
     proc = fork();
@@ -464,8 +429,8 @@ void piping(char *command)
 
     else if (proc == 0)
     {
-      //printf("%s\n", pipe_args[j]);
-      
+      // printf("%s\n", pipe_args[j]);
+
       dup2(fd, 0);
 
       if (pipe_args[j + 1] != NULL)
@@ -486,7 +451,6 @@ void piping(char *command)
     }
   }
 }
-
 
 /*
 compares command array with arg_buffer and
@@ -519,15 +483,15 @@ int command_handler(char **commands, int commands_size, char **arg_buffer, int b
       {
         copy_starting(arg_buffer, command_buff, i);
         found = 1;
-	if(strcmp(commands[j], "&") == 0){
-	  status = 0;
-	}
+        if (strcmp(commands[j], "&") == 0)
+        {
+          status = 0;
+        }
         if (strcmp(commands[j], "|") == 0)
         {
           status = 1;
         }
-        else if (strcmp(commands[j], "<") == 0 || strcmp(commands[j], ">") == 0
-		 || strcmp(commands[j], "2>") == 0 || strcmp(commands[j], ">>") == 0)
+        else if (strcmp(commands[j], "<") == 0 || strcmp(commands[j], ">") == 0)
         {
           status = 2;
         }
@@ -542,7 +506,6 @@ int command_handler(char **commands, int commands_size, char **arg_buffer, int b
 
   return status;
 }
-
 
 /*
 set the pointer at index and the pointers that come after
@@ -648,7 +611,7 @@ void get_args(char *str, char **arg_buffer, int arg_buffer_size)
 - works the same way as str_tok function
  */
 
-char *str_tok(char *str, const char *delim)
+char *str_tok_t(char *str, const char *delim)
 {
 
   static char *token;
@@ -697,4 +660,56 @@ char *str_tok(char *str, const char *delim)
     token++;
   }
   return str;
+}
+
+char *str_tok(char *str, const char *delim)
+{
+  char *spanp;
+  int c, sc;
+  char *tok;
+  static char *last;
+
+  if (str == NULL && (str = last) == NULL)
+    return (NULL);
+
+  /*
+   * Skip (span) leading delimiters (s += strspn(s, delim), sort of).
+   */
+cont:
+  c = *str++;
+  for (spanp = (char *)delim; (sc = *spanp++) != 0;)
+  {
+    if (c == sc)
+      goto cont;
+  }
+
+  if (c == 0)
+  { /* no non-delimiter characters */
+    last = NULL;
+    return (NULL);
+  }
+  tok = str - 1;
+
+  /*
+   * Scan token (scan for delimiters: s += strcspn(s, delim), sort of).
+   * Note that delim must have one NUL; we stop if we see that, too.
+   */
+  for (;;)
+  {
+    c = *str++;
+    spanp = (char *)delim;
+    do
+    {
+      if ((sc = *spanp++) == c)
+      {
+        if (c == 0)
+          str = NULL;
+        else
+          str[-1] = 0;
+        last = str;
+        return (tok);
+      }
+    } while (sc != 0);
+  }
+  /* NOTREACHED */
 }

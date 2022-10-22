d85afaa80311c4a7d7a5df73e7d95e591a5f5446
mojoegojoe <joe.spurway@hotmail.co.uk>
2022-10-22 05:21:02 +0000

    IO comments

--- mysh/input.c
+++ mysh/input.c
@@ -11,6 +11,15 @@ TODO:
 
 
  */
+
+/*
+   @function read_input
+   @method   takes a buffer of buffer_size to read in stdin from the user. 
+   @input    pointer to a buffer in memory of size char
+             int value for the size of the given buffer
+   @output    status code / fail bool
+   
+ */
 int read_input(char *buffer, int buffer_size)
 {
   int status;
